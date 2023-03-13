80f9fb1aff0646f26d64a21c9c2423ac093fad79
shubhamstrength <shubhamstrength@gmail.com>
2023-03-13 22:55:32 +0530

    initial test setup

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App />);
+
+    screen.debug();
+
+    // check if App components renders headline
+    expect(screen.findByText('Vite + React')).toBeTruthy();
+  });
+});
